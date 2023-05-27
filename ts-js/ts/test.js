258b878ddd2b5faffd8552f91c83311935cbb501
Man <2236879596@qq.com>
2023-05-27 16:57:15 +0800

    other: test

--- ts-js/ts/test.js
+++ ts-js/ts/test.js
@@ -5,8 +5,19 @@
  * @Author: Man
  * @Date: 2022-06-20 15:23:48
  * @LastModifiedBy: git config user.name
- * @LastEditTime: 2022-06-20 15:23:53
+ * @LastEditTime: 2023-04-26 15:36:29
  */
-const reverse = (s) => s.split('').reverse().join('');
-reverse('elon musk'); // -> 'ksum nole'
-console.log(reverse('elon musk'))
+// const reverse = (s) => s.split('').reverse().join('');
+// reverse('elon musk'); // -> 'ksum nole'
+// console.log(reverse('elon musk'))
+
+
+let array = [{id: 1,name:'lisi'}, {id: 2, name:'zhangsan'}];
+// console.log(array.filter((item) =>{return item.id >1}))
+let testID = array.reduce((pre, cur)=>{
+    // console.log(pre)
+    pre.push(cur.id)
+    return pre
+}, [])
+console.log(testID)
+
